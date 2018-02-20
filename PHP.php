702aaee829b3b1d5cff562db6fee1d8052f9316b
Ernest D. Peterson <ernest@ernestpeterson.com>
2018-02-20 05:27:28 -0600

    Started the MySQL section

--- PHP.php
+++ PHP.php
@@ -161,14 +161,71 @@
 
     </section>
 
+    <section class="hidden" id="fileUpload">
+
+        <br><br>
+        <h1>Upload an image to the server</h1>
+        <br><form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+        </form>
+
+        <?php
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+        ?>
+
+    </section>
+
 <script>
-    var buttonArray = ["formValidation", "mutiDimensionalArrays", "dateTime"];
+    var buttonArray = ["formValidation", "mutiDimensionalArrays", "dateTime", "fileUpload"];
 
     for (var buttonItterator = buttonArray.length - 1; buttonItterator >= 0; buttonItterator--) {
         $("#navigation").append("<button id='button" + buttonItterator + "' onclick='buttonClick(this.id)'>" + buttonArray[buttonItterator] + 
             "</button>");
     }
-
+// todo create a buttons array to control all this
     function buttonClick(buttonID) {
         // $("#dateTime, #formValidation, #mutiDimensionalArrays").toggleClass('hidden');
         switch (buttonID) {
@@ -180,6 +237,9 @@
                 break;
             case 'button2':
                 $("#dateTime").toggleClass('hidden');
+                break;
+            case 'button3':
+                $("#fileUpload").toggleClass('hidden');
                 break;
         }
     }
